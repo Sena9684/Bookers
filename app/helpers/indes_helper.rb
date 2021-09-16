182aa8a3f82af99d8e446dd119c8ba9fa0f7d4a7
Sena9684 <s1527067@i.softbank.jp>
2021-09-16 09:39:55 +0000

    初めてのコミット

---
+++ app/helpers/indes_helper.rb
@@ -0,0 +1,2 @@
+module IndesHelper
+end
---
+++ test/controllers/indes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IndesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
